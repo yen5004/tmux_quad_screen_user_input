d72034c383e647f2211cfb6b2858ec34e7df24fb
yen5004 <119946331+yen5004@users.noreply.github.com>
2022-12-29 16:58:56 -0500

    Update tmux_user_input.sh
    clean up

--- tmux_user_input.sh
+++ tmux_user_input.sh
@@ -1,45 +1,50 @@
 #!/bin/bash
 
+while true; do
+
 # Ask if user wants to customize Sess & window names
-read -p 'Customize session & window names? (y/n): " yn
+read -p 'Customize session & window names? (y/n): ' yn
 case $yn in
-    yes ) # Ask for sess & window names & store in var:
-            read -p 'Session Name: ', $SESSION
+    y ) # Ask for sess & window names & store in var:
+            read -p 'Session Name: ' SESSION
 
-            #Window 0 Name:
-            read -p 'Window 1 Name: ', $w0
-            
             #Window 1 Name:
-            read -p 'Window 2 Name: ', $w1
-
+            read -p 'Window 1 Name: ' w0
+            
             #Window 2 Name:
-            read -p 'Window 3 Name: ', $w2
+            read -p 'Window 2 Name: ' w1
 
             #Window 3 Name:
-            read -p 'Window 4 Name: ', $w3
+            read -p 'Window 3 Name: ' w2
 
             #Window 4 Name:
-            read -p 'Window 5 Name: ', $w4;;
+            read -p 'Window 4 Name: ' w3
+
+            #Window 5 Name:
+            read -p 'Window 5 Name: ' w4
+            break;;
     
-    no ) # Set session name
-         $SESSION= 'Pentest'
+    n ) # Set session name
+         SESSION= 'Pentest'
 
-         #Window 0 Name:
-         $w0='Recon'
-         
          #Window 1 Name:
-         $w1='Enumeration'
-
+         w0='Recon'
+         
          #Window 2 Name:
-         $w2='Exploit'
+         w1='Enumeration'
 
          #Window 3 Name:
-         $w3='Post Exploit'
+         w2='Exploit'
 
-          #Window 4 Name:
-          $w4='Report';;
+         #Window 4 Name:
+         w3='Post Exploit'
+
+         #Window 5 Name:
+         w4='Report'
+         break;;
     * ) exit 1;;
-
+esac
+done
 # Grab session name
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 
@@ -51,30 +56,24 @@ then
     # Start new window
     tmux new-window -t $SESSION:0 -n $w0
       # Create var for pipe-pane
-      $sip=tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
+      sip=tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
       # Logging Pane 0
-      # Try new code var instead of this line below
-      #tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
       $sip 
     # Start new window
     tmux new-window -t $SESSION:1 -n $w1
       #Logging Pane 1
-      #tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
       $sip
     #Start new window
     tmux new-window -t $SESSION:2 -n $w2
       #Logging Pane 2
-      #tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
       $sip
     #Start new window
     tmux new-window -t $SESSION:3 -n $w3
       #Logging Pane 3
-      #tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
       $sip
     #Start new window
     tmux new-window -t $SESSION:4 -n $w4
       #Logging Pane 4
-      #tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
       $sip
       
     #Name Windows and start zsh
