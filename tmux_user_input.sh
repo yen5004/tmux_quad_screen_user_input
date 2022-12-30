33dc073543c9faa912d9abdd20e8f108061c3777
yen5004 <119946331+yen5004@users.noreply.github.com>
2022-12-30 11:51:59 -0500

    Update tmux_user_input.sh
    fixed variables

--- tmux_user_input.sh
+++ tmux_user_input.sh
@@ -5,43 +5,45 @@ while true; do
 # Ask if user wants to customize Sess & window names
 read -p 'Customize session & window names? (y/n), press '*' to exit: ' yn
 case $yn in
-    y ) # Ask for sess & window names & store in var:
-            read -p 'Session Name: ' SESSION
+    y ) echo "No spaces in name or use underscore!"
+        sleep 2s
+        # Ask for sess & window names & store in var:
+        read -p 'Session Name: ' SESSION
 
-            #Window 1 Name:
-            read -p 'Window 1 Name: ' w0
+        #Window 1 Name:
+        read -p 'Window 1 Name: ' w0
             
-            #Window 2 Name:
-            read -p 'Window 2 Name: ' w1
+        #Window 2 Name:
+        read -p 'Window 2 Name: ' w1
+         
+        #Window 3 Name:
+        read -p 'Window 3 Name: ' w2
+         
+        #Window 4 Name:
+        read -p 'Window 4 Name: ' w3
 
-            #Window 3 Name:
-            read -p 'Window 3 Name: ' w2
-
-            #Window 4 Name:
-            read -p 'Window 4 Name: ' w3
-
-            #Window 5 Name:
-            read -p 'Window 5 Name: ' w4
-            break;;
+        #Window 5 Name:
+        read -p 'Window 5 Name: ' w4
+        break;;
     
     n ) # Set default naming schema
-         SESSION= 'Pentest'
+        SESSION= 'Pentest'
 
-         #Window 1 Name:
-         w0='Recon'
+        #Window 1 Name:
+        w0='Recon'
          
-         #Window 2 Name:
-         w1='Enumeration'
+        #Window 2 Name:
+        w1='Enumeration'
 
-         #Window 3 Name:
-         w2='Exploit'
+        #Window 3 Name:
+        w2='Exploit'
 
-         #Window 4 Name:
-         w3='Post Exploit'
+        #Window 4 Name:
+        w3='Post Exploit'
 
-         #Window 5 Name:
-         w4='Report'
-         break;;
+        #Window 5 Name:
+        w4='Report'
+        break;;
     * ) exit 1;;
 esac
 done
@@ -56,22 +58,22 @@ then
     tmux new-session -d -s $SESSION
     
     # Logging var shortcut
-    sip=tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
+    sip=$(tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P')
     
     # Start new window & log pane
-    tmux new-window -t $SESSION:0 -n $w0 && $sip
+    tmux new-window -a -t $SESSION:0 -n $w0 && $sip
        
     # Start new window & log pane
-    tmux new-window -t $SESSION:1 -n $w1 && $sip
+    tmux new-window -a -t $SESSION:1 -n $w1 && $sip
       
     #Start new window & log pane
-    tmux new-window -t $SESSION:2 -n $w2 && $sip
+    tmux new-window -a -t $SESSION:2 -n $w2 && $sip
     
     #Start new window & log pane
-    tmux new-window -t $SESSION:3 -n $w3 && $sip
+    tmux new-window -a -t $SESSION:3 -n $w3 && $sip
     
     #Start new window & log pane
-    tmux new-window -t $SESSION:4 -n $w4 && $sip
+    tmux new-window -a -t $SESSION:4 -n $w4 && $sip
       
     #Name Windows and start zsh
     tmux rename-window -t 0 $w0
