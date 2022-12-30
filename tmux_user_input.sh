44a62c88251942d2f844f142ea1e5e3b997ecbab
yen5004 <119946331+yen5004@users.noreply.github.com>
2022-12-30 10:42:06 -0500

    Update tmux_user_input.sh
    shorten code

--- tmux_user_input.sh
+++ tmux_user_input.sh
@@ -3,7 +3,7 @@
 while true; do
 
 # Ask if user wants to customize Sess & window names
-read -p 'Customize session & window names? (y/n): ' yn
+read -p 'Customize session & window names? (y/n), press '*' to exit: ' yn
 case $yn in
     y ) # Ask for sess & window names & store in var:
             read -p 'Session Name: ' SESSION
@@ -24,7 +24,7 @@ case $yn in
             read -p 'Window 5 Name: ' w4
             break;;
     
-    n ) # Set session name
+    n ) # Set default naming schema
          SESSION= 'Pentest'
 
          #Window 1 Name:
@@ -45,6 +45,7 @@ case $yn in
     * ) exit 1;;
 esac
 done
+
 # Grab session name
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 
@@ -53,28 +54,24 @@ if ["$SESSIONEXISTS" = ""]
 then
     # Start new session with our name
     tmux new-session -d -s $SESSION
-    # Start new window
-    tmux new-window -t $SESSION:0 -n $w0
-      # Create var for pipe-pane
-      sip=tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
-      # Logging Pane 0
-      $sip 
-    # Start new window
-    tmux new-window -t $SESSION:1 -n $w1
-      #Logging Pane 1
-      $sip
-    #Start new window
-    tmux new-window -t $SESSION:2 -n $w2
-      #Logging Pane 2
-      $sip
-    #Start new window
-    tmux new-window -t $SESSION:3 -n $w3
-      #Logging Pane 3
-      $sip
-    #Start new window
-    tmux new-window -t $SESSION:4 -n $w4
-      #Logging Pane 4
-      $sip
+    
+    # Logging var shortcut
+    sip=tmux pipe-pane -o 'cat >>~/tmux_output.#S:#I-#P'
+    
+    # Start new window & log pane
+    tmux new-window -t $SESSION:0 -n $w0 && $sip
+       
+    # Start new window & log pane
+    tmux new-window -t $SESSION:1 -n $w1 && $sip
+      
+    #Start new window & log pane
+    tmux new-window -t $SESSION:2 -n $w2 && $sip
+    
+    #Start new window & log pane
+    tmux new-window -t $SESSION:3 -n $w3 && $sip
+    
+    #Start new window & log pane
+    tmux new-window -t $SESSION:4 -n $w4 && $sip
       
     #Name Windows and start zsh
     tmux rename-window -t 0 $w0
